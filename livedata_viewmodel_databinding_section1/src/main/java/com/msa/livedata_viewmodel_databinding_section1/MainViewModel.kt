d407fbe4adc29ececad9812f2b950ce1ecc27f1d
ali solimani <solimaniali90@gmail.com>
2022-05-20 16:41:50 +0430

    LiveData DataBinding

--- livedata_viewmodel_databinding_section1/src/main/java/com/msa/livedata_viewmodel_databinding_section1/MainViewModel.kt
+++ livedata_viewmodel_databinding_section1/src/main/java/com/msa/livedata_viewmodel_databinding_section1/MainViewModel.kt
@@ -12,12 +12,23 @@ class MainViewModel:ViewModel() {
     get() = textplus
 
 
+    private var totext=MutableLiveData<String>()
+    val textData:LiveData<String>
+    get() = totext
+
+    @Bindable
+    val inputText=MutableLiveData<String>()
     @Bindable
     val username=MutableLiveData<String>()
 
     init {
         textplus.value=0
         username.value="Steve"
+    }
+
+    fun setText(){
+        var input:String=inputText.value!!.toString()
+        totext.value=input
     }
     fun setAddPlus(){
         textplus.value=(textplus.value)?.plus(1)
